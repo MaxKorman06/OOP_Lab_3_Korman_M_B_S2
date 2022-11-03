85ef79e31462c0b58532374a269e09fffbba1bc0
Максим Корман <mkorman359@gmail.com>
2022-11-03 23:43:20 +0200

    Add project files.

---
+++ OOP_Lab_3_Korman_M_B_S2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Lab_3_Korman_M_B_S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
